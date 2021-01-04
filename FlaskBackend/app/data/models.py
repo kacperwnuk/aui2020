48444f3f99d3709dcb415e3381068fa883889818
Kacper Klimczuk <kacper.k2@o2.pl>
2021-01-04 22:54:53 +0100

    Corrected models

--- FlaskBackend/app/data/models.py
+++ FlaskBackend/app/data/models.py
@@ -4,26 +4,21 @@ from flask_security import UserMixin, RoleMixin
 
 from .. import database as db
 
-users_codes = db.Table('users_codes',
-                       db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-                       db.Column('code_id', db.Integer(), db.ForeignKey('httpcode.id')))
-users_events = db.Table('users_events',
-                        db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-                        db.Column('event_id', db.Integer(), db.ForeignKey('userevent.id')))
-
 
 class HttpCode(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     code = db.Column(db.Integer, unique=False)
     time = db.Column(db.DateTime, unique=False, default=datetime.now)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 class UserEvent(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     site = db.Column(db.String(20), unique=False)
-    enter_time = db.Column(db.DateTime, unique=False)
-    exit_time = db.Column(db.DateTime, unique=False)
-    time_spent = db.Column(db.DateTime, unique=False)  # czy moze jako integer w sek??
+    enter_time = db.Column(db.DateTime, unique=False, default=datetime.now)
+    exit_time = db.Column(db.DateTime, unique=False, default=datetime.now)
+    time_spent = db.Column(db.Integer, unique=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
 roles_users = db.Table('roles_users',
@@ -38,10 +33,8 @@ class User(db.Model, UserMixin):
     active = db.Column(db.Boolean())
     roles = db.relationship('Role', secondary=roles_users,
                             backref=db.backref('users', lazy='dynamic'))
-    http_codes = db.relationship('HttpCode', secondary=users_codes,
-                                 backref=db.backref('users', lazy='dynamic'))
-    events = db.relationship('UserEvent', secondary=users_events,
-                             backref=db.backref('users', lazy='dynamic'))
+    http_codes = db.relationship('HttpCode', backref='users_http_code')
+    events = db.relationship('UserEvent', backref='users_event')
 
 
 class Role(db.Model, RoleMixin):
